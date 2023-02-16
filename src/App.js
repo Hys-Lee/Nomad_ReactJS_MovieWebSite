23f7db0d6be590751791bdcee85418d67d235188
Hys-Lee <dltkdgus3275@gmail.com>
2023-02-16 20:42:22 +0900

    Router컴포넌트에 basename속성값 추가 -> detail로 안넘어가서.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Details from "./Movie/router/Details"
 function App() {
 
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Switch>{/* // Swtich는 Route를 찾는데, Route는 url의 '/~~'와 같이
         //슬래시를 포함하는 뒷부분을 의미한다.*/}
         <Route path="/movie/:movie_id" > {/* ':'을 붙이면 url에서 해당 위치에 있는 값을 변수로 받을 수 있다. */}
